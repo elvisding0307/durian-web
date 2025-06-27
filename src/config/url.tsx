f3314023d026a03532f8a14fb8e2c90b07bf971b
Elvis Ding <elvisding0307@gmail.com>
2025-06-28 01:08:32 +0800

    fix: 修正url

--- src/config/url.tsx
+++ src/config/url.tsx
@@ -1,3 +1,2 @@
-export const BASE_URL = 'http://api.durian.xyz:7224';
+export const BASE_URL = 'https://localhost:7224/api';
 export const API_BASE_URL = BASE_URL + '/v1';
-export const AUTH_BASE_URL = API_BASE_URL + '/auth';
